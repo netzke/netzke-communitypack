e05caf8d907f07559a8039555f99c1bd4464ca5d
Georg Meyer <georgmeyer83@googlemail.com>
2012-09-03 23:17:56 +0200

    Remove old test app

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '6ccc50b0fe2b3268be36b9186e90774af4162b2a35c0deef616b1016459802f9b0b2cf202e231aba6d171336fef7396f3600c638ff62380954f396e696f5ea37'
