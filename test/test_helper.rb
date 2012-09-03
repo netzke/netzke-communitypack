11478033adbbf91ff85190fdc3838d48ac818514
Georg Meyer <georgmeyer83@googlemail.com>
2012-09-03 23:16:23 +0200

    Update path to new test app

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require File.expand_path("../communitypack_test_app/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
 Rails.backtrace_cleaner.remove_silencers!
