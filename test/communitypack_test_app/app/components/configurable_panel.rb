3075dd1ce06953cf5b6702e2139d9c6249ba433c
nomadcoder <nmcoder@gmail.com>
2012-12-24 09:13:30 +0700

    Add a couple more test cases to WorkspaceTester

---
+++ test/communitypack_test_app/app/components/configurable_panel.rb
@@ -0,0 +1,26 @@
+class ConfigurablePanel < Netzke::Base
+  action :update_html
+
+  js_configure do |c|
+    c.title = "ConfigurablePanel"
+    c.on_update_html = <<-JS
+      function(){
+        this.updateHtmlFromServer();
+      }
+    JS
+  end
+
+  def configure(c)
+    c.html = "Original HTML"
+    c.bbar = [:update_html]
+    super
+  end
+
+  def self.server_side_config_options
+    super << :update_text
+  end
+
+  endpoint :update_html_from_server do |params, this|
+    this[:update] = [config[:update_text] || "HTML received from server"]
+  end
+end
--- test/communitypack_test_app/app/components/workspace_tester.rb
+++ test/communitypack_test_app/app/components/workspace_tester.rb
@@ -2,11 +2,13 @@ class WorkspaceTester < Netzke::Base
   action :load_some_panel
   action :load_another_panel
   action :load_book_explorer
+  action :load_configurable_panel
+  action :load_grid
 
   def configure(c)
     super
     c.items = [:workspace]
-    c.bbar = [:load_some_panel, :load_another_panel, :load_book_explorer]
+    c.bbar = [:load_some_panel, :load_another_panel, :load_book_explorer, :load_configurable_panel, :load_grid]
   end
 
   component :workspace do |c|
@@ -20,14 +22,24 @@ class WorkspaceTester < Netzke::Base
         this.netzkeGetComponent('workspace').loadInTab("SomePanel");
       }
     JS
-    c.on_load_another_panel = <<-JS
+
+    [:another_panel, :book_explorer].each do |cmp|
+      c.send(:"on_load_#{cmp}=", <<-JS)
+        function(){
+          this.netzkeGetComponent('workspace').loadInTab("#{cmp.to_s.camelize}", {newTab: true});
+        }
+      JS
+    end
+
+    c.on_load_configurable_panel = <<-JS
       function(){
-        this.netzkeGetComponent('workspace').loadInTab("AnotherPanel", {newTab: true});
+        this.netzkeGetComponent('workspace').loadInTab("ConfigurablePanel", {newTab: true, config: {update_text: 'HTML from config'}});
       }
     JS
-    c.on_load_book_explorer = <<-JS
+
+    c.on_load_grid = <<-JS
       function(){
-        this.netzkeGetComponent('workspace').loadInTab("BookExplorer", {newTab: true});
+        this.netzkeGetComponent('workspace').loadInTab("BookGridWithColumnActions", {newTab: true, config: {strong_default_attrs: {title: 'Overridden'}}});
       }
     JS
   end
