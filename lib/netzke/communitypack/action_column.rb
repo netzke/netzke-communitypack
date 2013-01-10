cd77c4372a558757a6a6d12d16543e6195675786
nomadcoder <nmcoder@gmail.com>
2013-01-10 12:40:09 +0700

    Update ActionColumn to work with latest Core

--- lib/netzke/communitypack/action_column.rb
+++ lib/netzke/communitypack/action_column.rb
@@ -13,21 +13,26 @@ module Netzke
       def augment_column_config(c)
         if c[:type] == :action
           c.xtype = :netzkeactioncolumn
-          c[:getter] = lambda do |r|
-            c.actions.map do |a|
-              a = {name: a} if a.is_a?(Symbol)
-              a.tap do |a|
-                a[:tooltip] ||= a[:name].to_s.humanize
-                a[:icon] ||= a[:name].to_sym
-                a[:handler] ||= "on_#{a[:name]}"
 
-                a[:icon] = "#{Netzke::Core.icons_uri}/#{a[:icon]}.png" if a[:icon].is_a?(Symbol)
-              end
-            end.to_nifty_json
+          c[:getter] = lambda do |r|
+            c.actions.map {|a| build_action_config(a)}.netzke_jsonify.to_json
           end
         end
 
         super
+      end
+
+    private
+
+      def build_action_config(a)
+        a = {name: a} if a.is_a?(Symbol)
+        a.tap do |a|
+          a[:tooltip] ||= a[:name].to_s.humanize
+          a[:icon] ||= a[:name].to_sym
+          a[:handler] ||= "on_#{a[:name]}"
+
+          a[:icon] = "#{Netzke::Core.icons_uri}/#{a[:icon]}.png" if a[:icon].is_a?(Symbol)
+        end
       end
     end
   end
